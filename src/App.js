d28ce2326e8d8f4d284cc099a6ca8c9299629c99
tsensei <tjsiam019@gmail.com>
2021-05-15 09:45:20 +0600

    All working TODO:Stop rerendering when input changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [country, setCountry] = useState("Bangladesh");
+  const [apiData, setApiData] = useState([]);
+  const [countryChanged, setCountryChanged] = useState(false);
+  const date = new Date();
+
+  useEffect(() => {
+    const getData = () => {
+      fetch(`https://coronavirus-19-api.herokuapp.com/countries/${country}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setApiData(result);
+        });
+    };
+
+    getData();
+  }, [countryChanged]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <div className="covid-svg ico">
+            <img
+              src="images/covid-19.svg"
+              className="nav-icon"
+              alt="covid-icon"
+            />
+          </div>
+          <div className="search-input">
+            <input
+              type="text"
+              className="search-input"
+              onChange={(e) => {
+                setCountry(e.target.value);
+              }}
+              value={country}
+            />
+          </div>
+          <div
+            className="search-icon ico"
+            onClick={() => {
+              setCountryChanged(!countryChanged);
+            }}
+          >
+            <img
+              src="images/search.svg"
+              className="nav-icon"
+              alt="search-icon"
+            />
+          </div>
+          <div className="location-icon ico">
+            <img
+              src="images/location.svg"
+              className="nav-icon"
+              alt="location-icon"
+            />
+          </div>
+        </div>
+
+        {typeof apiData.cases !== "undefined" ? (
+          <div className="container">
+            <div className="country-header">{apiData.country}</div>
+            <div className="current-time">
+              {date.toLocaleString("default", { month: "long" })}{" "}
+              {date.getDate()},{date.getFullYear()}
+            </div>
+            <div className="total-cases">
+              <div className="tc-header">Total Cases</div>
+              <div className="tc-cases">{apiData.cases}</div>
+            </div>
+            <div className="card active-cases">
+              <div className="card-header">Active Cases</div>
+              <div className="card-body">{apiData.active}</div>
+            </div>
+            <div className="card recovered-cases">
+              <div className="card-header">Recovered</div>
+              <div className="card-body">{apiData.recovered}</div>
+            </div>
+            <div className="card death-cases">
+              <div className="card-header">Death</div>
+              <div className="card-body">{apiData.deaths}</div>
+            </div>
+          </div>
+        ) : (
+          <h2>Loading</h2>
+        )}
+
+        <div className="footer">Made with ❤️️ by tsensei</div>
+      </main>
     </div>
   );
 }
